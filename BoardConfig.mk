7698e6be00063c4c6f634be9da2c2111fc093967
ksauraj <gitsauraj@gmail.com>
2021-12-13 06:19:08 +0000

    root_files: sys_oplus_mssi_64_cn-user-11-RP1A.200720.011-1607914664672-release-keys-Thu-Mar-17-03-16-17-UTC-2022

---
+++ BoardConfig.mk
@@ -0,0 +1,9 @@
+DEVICE_PATH := device/realme/even
+BOARD_VENDOR := realme
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2022-02-05
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
+-include vendor/realme/even/BoardConfigVendor.mk
