5bd5e6f6132e9c38393f515eb46a086c11f3e4d6
noobyysauraj <gitsauraj@gmail.com>
2022-03-17 03:58:41 +0000

    sys_oplus_mssi_64_cn-user-11-RP1A.200720.011-1607914664672-release-keys
    Signed-off-by: noobyysauraj <gitsauraj@gmail.com>

---
+++ device.mk
@@ -0,0 +1,11 @@
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    ueventd.qcom.rc \
+
+# Inherit vendor
+$(call inherit-product, vendor/realme/even/even-vendor.mk)
